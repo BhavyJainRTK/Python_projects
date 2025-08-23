a2a117c4b2cb901f4ee8e9baa846c13249ed11b7
BhavyJainRTK <bhavy2142006@gmail.com>
2025-08-23 22:21:04 +0530

    add new program

---
+++ 9_giving_grade_to_students.py
@@ -0,0 +1,13 @@
+marks = float(input("Enter your marks : "))
+
+if(marks >= 90):
+    print("GRADE = A",)
+
+if((marks >= 80)and(marks < 90)):
+    print("GRADE = B",)
+
+if((marks >= 70)and(marks < 80)):
+    print("GRADE = C",)
+
+if(marks < 70):
+    print("GRADE = D",)
