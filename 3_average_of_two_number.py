a45f12d3a8134f38bbf274fe953031bf1e20eb6e
BhavyJainRTK <bhavy2142006@gmail.com>
2025-08-23 17:50:45 +0530

    add new program

---
+++ 3_average_of_two_number.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter the first number : "))
+num2 = float(input("Enter the second number : "))
+
+average = num1 + num2
+average /= 2
+
+print("average of both number is > ",average)
