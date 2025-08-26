f9128895848a6a8b9191b70b22c6eeba8094ca6b
BhavyJainRTK <bhavy2142006@gmail.com>
2025-08-26 22:38:55 +0530

    add new program

---
+++ 14_making_list_from_input_from_user.py
@@ -0,0 +1,13 @@
+print("Enter your thre favorite games")
+
+all_games = []
+
+game1 = input("enter 1st game > ")
+game2 = input("enter 2nd game > ")
+game3 = input("enter 3rd game > ")
+
+all_games.append(game1)
+all_games.append(game2)
+all_games.append(game3)
+
+print(all_games)
