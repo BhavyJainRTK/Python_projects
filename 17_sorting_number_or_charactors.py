f6ad6e82646ed3d2c7e6c9937eeccde965aec898
BhavyJainRTK <bhavy2142006@gmail.com>
2025-08-26 23:03:20 +0530

    add new program

---
+++ 17_sorting_number_or_charactors.py
@@ -0,0 +1,12 @@
+print("Enter five numbers or charactors")
+
+list = []
+
+list.append(input("Enter 1st element > "))
+list.append(input("Enter 2nd element > "))
+list.append(input("Enter 3rd element > "))
+list.append(input("Enter 4th element > "))
+list.append(input("Enter 5th element > "))
+
+list.sort()
+print(list)
